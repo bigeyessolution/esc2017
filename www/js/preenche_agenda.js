5c9bd1db8215b812dd6e9717d672506688b0e0ee
Washington Azevedo <azevedo@localhost.localdomain>
2017-05-01 19:48:41 -0300

    Agenda Atualizada e CSS Modificado

--- www/js/preenche_agenda.js
+++ www/js/preenche_agenda.js
@@ -2,12 +2,12 @@ var agenda = false && localStorage.agenda ? JSON.parse(localStorage.agenda) :
 {
 	"version": 25,
 	"agenda10" : [
-		{"salahora": "8:30", "titulo": "Credenciamento", "info": "", "divisao": true},
+		{"salahora": "8:00", "titulo": "Credenciamento", "info": "", "divisao": true},
 		{
 			"collapsible": true,
-			"class": "ui-be-plenary-session",
+			"class": "ui-be-workshop-session",
 			"descricao": "Workshop de Engenharia",
-			"salahora": "9:00",
+			"salahora": "8:30",
 			"itens": [
 				{"salahora": "Lorangerie I - 8:30", "titulo": "Composite Design", "info": "Prof. Airton Nabarrete - ITA e Eduardo Araújo - ESSS"},
 				{"salahora": "Lorangerie II - 8:30", "titulo": "Risk analysis: The effect of small scale geometry in the modelling of accidental gas dispersion using CFD", "info": "Prof. Savio Vianna - Unicamp"},
@@ -19,92 +19,153 @@ var agenda = false && localStorage.agenda ? JSON.parse(localStorage.agenda) :
 		{
 			"collapsible": true,
 			"class": "ui-be-plenary-session",
-			"descricao": "Plenária",
-			"salahora": "10:40",
+			"descricao": "Sessões Plenárias",
+			"salahora": "10:00",
 			"itens": [
-				{"salahora": "10:40", "titulo": "Aplicação da Simulação Computacional na Engenharia de Equipamentos Subsea", "info": "Hugo Almeida"},
-				{"salahora": "11:10", "titulo": "Software customizado para aplicações especiais em Petróleo e Gás", "info": "Leonardo Paes Rangel"},
-				{"salahora": "11:40", "titulo": "CFDOIL - Trajetória e Homenagens", "info": "Clovis R. Maliska"}
+				{"salahora": "Versailles - 10:00", "titulo": "Opening", "info": "Clovis Maliska Jr., ESSS"},
+				{"salahora": "Versailles - 10:10", "titulo": "Simulation-Based Digital Twins and the Industrial Internet of Things", "info": "Sameer Kher, ANSYS"},
+				{"salahora": "Versailles - 10:40", "titulo": "The use of Numerical Simulation in ARCELORMITTAL Group", "info": "Rafael Sartim, ArcelorMittal"},
+				{"salahora": "Versailles - 11:10", "titulo": "Putting the Industrial Internet into practice", "info": "J. Rizzo Hahn Filho, ABII"},
+				{"salahora": "Versailles - 11:40", "titulo": "Plenary Session 4", "info": ""}
 			]
 		},
-		{"salahora": "12:30", "titulo": "Almoço", "info": "", "divisao": true},
+		{"salahora": "13:30 - 14:30", "titulo": "Sessões Práticas", "info": "", "divisao": true},
 		{
 			"collapsible": true,
-			"class": "ui-be-fluidodinamica-1",
-			"descricao": "Fluidodinâmica I",
-			"salahora": "13:00",
+			"class": "ui-be-pratic-session",
+			"descricao": "Lorangerie I - Structural Mechanics",
+			"salahora": "13:30",
 			"itens": [
-				{"salahora": "13:00", "titulo": "Simulação de interação fluido-estrutural-acústica do sistema de flare de alta pressão da P58", "info": "Ana Paula Faria e Raphael Bacchi, ESSS e Ricardo Serfaty, PETROBRAS"},
-				{"salahora": "13:30", "titulo": "Avaliação da injeção de spray em sistemas de topo de torres de destilação para controlar corrosão", "info": "Guilherme Pimentel de Maria da Silva e Gustavo José Simões, PETROBRAS"},
-				{"salahora": "14:00", "titulo": "Utilização de fluidodinâmica computacional (CFD) para estudos de tochas (Flare)", "info": "Daniel Evandro Ludwig, Marcos Antonio Pinto do Amaral, Mauro Cresta de Barros Dolinsky e Renata Dias Ambrósio, PETROBRAS"},
-				{"salahora": "14:30", "titulo": "Utilização de CFD em engenharia oceânica", "info": "Daniel Fonseca de Carvalho e Silva, PETROBRAS"},
-				{"salahora": "15:00", "titulo": "Avaliação fluidodinâmica do conjunto flare/soprador de ar de uma unidade de processo", "info": "João Victor Barbosa Alves e Paulo Roberto Pagot, PETROBRAS/Radix"}
-			]
-		},
-		{
-			"collapsible": true,
-			"class": "ui-be-fluidodinamica-2",
-			"descricao": "Fluidodinâmica II",
-			"salahora": "13:00",
-			"itens": [
-				{"salahora": "13:00", "titulo": "Estudo em CFD do desempenho de ejetores", "info": "Michele Pedroso, PETROBRAS"},
-				{"salahora": "13:30", "titulo": "Estudo de CFD de sistemas reativos compressíveis", "info": "Eduardo Oliveira, PETROBRAS"},
-				{"salahora": "14:00", "titulo": "Avaliação da deposição de partículas em tubulações de uma instalação petroquímica usando CFD", "info": "Carlos Caruso, BRASKEM"},
-				{"salahora": "14:30", "titulo": "Simulação fluidodinâmica da injeção de diesel para a preservação de uma flowline preenchida com óleo em águas ultraprofundas", "info": "Gabriel Goes, Queiroz Galvão"},
-			    {"salahora": "15:00", "titulo": "Aplicação de fluidodinâmica computacional e sistemas de reatores na avaliação de equipamentos de processo", "info": "Ana Paula Faria, ESSS"}
+				{"salahora": "13:30", "titulo": "Topology Optimization Analysis in Workbench", "info": ""},
+				{"salahora": "13:50", "titulo": "Workflow and Usability Features in Workbench Mechanical", "info": ""},
+				{"salahora": "14:10", "titulo": "Rigid Body Dynamics Systems with Simplorer Integration", "info": ""}
 			]
 		},
 		{
 			"collapsible": true,
-			"class": "ui-be-estrutural",
-			"descricao": "Estrutural e Multifísica",
-			"salahora": "13:00",
+			"class": "ui-be-pratic-session",
+			"descricao": "Lorangerie II - Fluid Dynamics",
+			"salahora": "13:30",
 			"itens": [
-				{"salahora": "13:00", "titulo": "Internet das coisas industrial: Explorando o potencial dos produtos e serviços conectados na indústria de O&G", "info": "Juliano Mologni, ESSS"},
-				{"salahora": "13:30", "titulo": "Simulação acústica de silenciadores de um compressor de parafusos", "info": "Leonardo Junqueira Mattana Saturnino e Mateus Louis Vieira, PETROBRAS"},
-				{"salahora": "14:00", "titulo": "Determinação da mínima perna de solda requerida em conexões de pequeno diâmetro com niples tipo meia luva", "info": "Hervandil Morosini Sant'Anna, PETROBRAS"},
-				{"salahora": "14:30", "titulo": "Inovando no Projeto de Engenharia de Torres com Simulação Multifísica", "info": "Juliano Mologni e Mauricio Pacheco, ESSS e Batista Luz, Leo Snovareski e Ricardo Damian, Jet Towers"},
-				{"salahora": "15:00", "titulo": "Absorção de energia de impacto em chapas de aço", "info": "Mario Castro, Technip"}
-			]
-		},
-		{"salahora": "15:30", "titulo": "Coffee-Break", "info": "", "divisao": true},
-		{
-			"collapsible": true,
-			"class": "ui-be-fluidodinamica-1",
-			"descricao": "Fluidodinâmica I",
-			"salahora": "16:00",
-			"itens": [
-				{"salahora": "16:00", "titulo": "Desafios tecnológicos e a contribuição de técnicas de simulação fluidodinâmica em projetos de construção de poços", "info": "Thiago Judson, PETROBRAS"},
-				{"salahora": "16:30", "titulo": "Análise do escoamento em trocadores WHRU através de fluidodinâmica computacional", "info": "Karolline Ropelato e Raphael Bacchi, ESSS e Francisco Miller e Victor souza, PETROBRAS"},
-				{"salahora": "17:00", "titulo": "Simulação de CFD de ciclones de entrada do separador de produção de uma plataforma", "info": "Fabricio Soares da Silva e João Victor Barbosa Alves, PETROBRAS/Radix"},
-				{"salahora": "17:30", "titulo": "Análise de combustíveis e sistemas reativos com ANSYS Reaction Workbench e ANSYS Chemkin Pro", "info": "Raphael Bacchi, ESSS"}
+				{"salahora": "13:30", "titulo": "Co-simulação System and ROM using Simplorer", "info": ""},
+				{"salahora": "13:50", "titulo": "Multiphase Flow", "info": ""},
+				{"salahora": "14:10", "titulo": "Overset Mesh using ANSYS Fluent", "info": ""}
 			]
 		},
 		{
 			"collapsible": true,
-			"class": "ui-be-fluidodinamica-2",
-			"descricao": "Fluidodinâmica II",
-			"salahora": "16:00",
+			"class": "ui-be-pratic-session",
+			"descricao": "Malmaison - Electromagnetics",
+			"salahora": "13:30",
 			"itens": [
-				{"salahora": "16:00", "titulo": "Avaliação de falha por corrosão-erosão em linha de hidrocarboneto", "info": "Diogo Dantas Ribeiro, Fernando César Lopes de Oliveira e Franciele Meira Ferreira, PETROBRAS"},
-				{"salahora": "16:30", "titulo": "CFD como ferramenta para alocação de detectores de gás: cuidados, desafios e incertezas associadas", "info": "Bruno Barbabela, Bureau Veritas"},
-				{"salahora": "17:00", "titulo": "Metodologia baseada em escoamento para análise da operação de bombeio mecânico", "info": "Daniel da Cunha Ribeiro, UFES"},
-				{"salahora": "17:30", "titulo": "Simulação fluidodinâmica de um sistema de flare", "info": "Karolline Ropelato e Rodrigo Peralta Muniz Moreira, ESSS e Frederico Catalao de Carvalho, PETROBRAS"}
+				{"salahora": "13:30", "titulo": "Simulation of Antennas on Platform using HFSS SBR+", "info": ""},
+				{"salahora": "13:50", "titulo": "Analysis of the magnetorestriction effect in transformers", "info": ""},
+				{"salahora": "14:10", "titulo": "TDM (Time Decomposition Method)", "info": ""}
 			]
 		},
 		{
 			"collapsible": true,
-			"class": "ui-be-estrutural",
-			"descricao": "Estrutural e Multifísica",
-			"salahora": "16:00",
+			"class": "ui-be-pratic-session",
+			"descricao": "Chambord - DEM &#38; Multiphysics",
+			"salahora": "13:30",
 			"itens": [
-				{"salahora": "16:00", "titulo": "Análise do comportamento de estacas torpedo em solos coesivos", "info": "José Renato, UFRJ"},
-				{"salahora": "16:30", "titulo": "Avaliação numérica da passagem de PIG raspador em trechos de circuito teste da Petrobras", "info": "Maurício Rangel Pacheco e Raphael David Aquilino Bacchi, ESSS e Eduardo Teixeira e André Maricato, PETROBRAS"},
-				{"salahora": "17:00", "titulo": "Exemplos de FEA para equipamentos submarinos: Comparação da abordagem de tensão local via DNVGL RP C203 / ANSYS ACT para a avaliação das tensões conforme API 17L", "info": "Hugo Almeida, Technip"},
-				{"salahora": "17:30", "titulo": "Avaliação de VIV aplicado a equipamentos submarinos com fluidodinâmica computacional", "info": "Bianca Teixeira, ESSS"}
+				{"salahora": "13:30", "titulo": "Rocky DEM 4 - New Features", "info": ""},
+				{"salahora": "13:50", "titulo": "Rocky DEM - CFD Coupling", "info": ""},
+				{"salahora": "14:10", "titulo": "Multiphysics AIM", "info": ""}
 			]
 		},
-		{"salahora": "18:00", "titulo": "Sorteio e Encerramento", "info": "", "divisao": true}
+		{"salahora": "14:30 - 17:40", "titulo": "Trabalhos Técnicos", "info": "", "divisao": true},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Lorangerie I - Structural Mechanics",
+			"salahora": "14:30",
+			"itens": [
+				{"salahora": "14:30", "titulo": "Assessment of the API 6AF sealing criterion compared to the one presented in ISO13268-7 standard", "info": "Guilherme Moura, TECHNIP FMC"},
+				{"salahora": "14:50", "titulo": "Implementation of custom material constitutive models in Ansys Mechanical using USERMAT subroutines", "info": "Lucas Breder Teixeira, Magnesita"},
+				{"salahora": "15:10", "titulo": "Spectral fatigue based on randon vibration simulation", "info": "Guilherme Espindola, EMBRACO"},
+				{"salahora": "15:30", "titulo": "Numerical Approach for Node Repositioning and its Benefits for Connecting Rod Structural Evaluation", "info": "Rafael Aguera, THYSSENKRUPP / Luiz Lima, ESSS"},
+				{"salahora": "16:20", "titulo": "Topology Optimization using ANSYS R18 at ZF do Brasil", "info": "Janaina Rodrigues / Sergio Ricardo Scabar, ZF do Brasil"},
+				{"salahora": "16:40", "titulo": "Acoustic noise modeling with excitations of electromagnetic forces on three phase induction motors", "info": "Julio Cesar da Silva, WEG"},
+				{"salahora": "17:00", "titulo": "Structural simulation applied to agricultural equipment", "info": "Állex Sander Cappellari / Rafael Kappaun / Róbinson João Jabuonski Júnior, STARA"},
+				{"salahora": "17:20", "titulo": "Use of aluminum foam to improve crashworthiness in automotive industry", "info": "Guilherme Rossi Reis, Felipe Biondo, MARCOPOLO"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Lorangerie II - Fluid Dynamics",
+			"salahora": "14:30",
+			"itens": [
+				{"salahora": "14:30", "titulo": "Use of dynamic similarity in the development of new hydraulics and their confirmation via Ansys CFX", "info": "Alison Arantes, KSB"},
+				{"salahora": "14:50", "titulo": "Simplified CFD model for the suction process of reciprocating compressor", "info": "Bruno Backes, EMBRACO"},
+				{"salahora": "15:10", "titulo": "Pump Improvement in a sold project", "info": "Ismael Schroer, Higra"},
+				{"salahora": "15:30", "titulo": "Heat Transfer Prediction in Hermetic Reciprocating Compressors", "info": "Jônatas Ferreira Lacerda, Tecumseh do Brasil"},
+				{"salahora": "16:20", "titulo": "CFD analysis of orifice plate with mass transfer", "info": "Gustavo Simões, PETROBRAS"},
+				{"salahora": "16:40", "titulo": "Evaluation of a marine propeller using computational fluid dynamics", "info": "Airton Patrício de Aquino Júnior / Jeferson Ferreira de Oliveira, CPN"},
+				{"salahora": "17:00", "titulo": "CFD analysis of fired furnace and flow induced distributions along pipes", "info": "Eduardo Oliveira, PETROBRAS"},
+				{"salahora": "17:20", "titulo": "Wind and Current Loads Determination on Offshore Rigs near Platforms", "info": "Daniel Fonseca, PETROBRAS"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Malmaison - Structural Mechanics",
+			"salahora": "14:30",
+			"itens": [
+				{"salahora": "14:30", "titulo": "Evaluation of the mechanical behavior of the chassis of a wheel loader", "info": "Bruno Tudeia, ISQ"},
+				{"salahora": "14:50", "titulo": "Stress and Deformation Analysis on high precision mirrors", "info": "Allan Gilmour Anderson Junior, LNLS"},
+				{"salahora": "15:10", "titulo": "Structural analysis of Chimney fixing system in the metal frame", "info": "Nelson Mateus Lorenzoni / José Cleber Rodrigues da Silva, YARA"},
+				{"salahora": "15:30", "titulo": "Railway wagon's floor structural analysis", "info": "Raidam Pereira Fernandes, VALE"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Chambord - DEM & Multiphysics",
+			"salahora": "14:30",
+			"itens": [
+				{"salahora": "14:30", "titulo": "Application of Discrete Element Model to the development of agricultural machinery and equipment in the sugar-acohol industry", "info": "Ed Carlos Alves Rocha, CTBE"},
+				{"salahora": "14:50", "titulo": "Analysis of a CaCO3 calciner using one-way coupilng between Ansys Fluent (CFD) and Rocky DEM", "info": "Renata Favalli, DYNAMIS"},
+				{"salahora": "15:10", "titulo": "Development of a New FPSO concept", "info": "Anderson Takehiro Oshiro, Petrobras"},
+				{"salahora": "15:30", "titulo": "How to gain prominence in the world market using knowledge and simulation tools", "info": "Marcelo Carneiro De Paiva, Inatel/ICC"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Malmaison - Fluid Dynamics",
+			"salahora": "16:20",
+			"itens": [
+				{"salahora": "16:20", "titulo": "Heat Transfer for Mixing Tanks (Traditional Method vs CFD Technology)", "info": "Alexandre Sampaio de Lima Malagodi, SEMCO"},
+				{"salahora": "16:40", "titulo": "Analysis of a fabric filter using CFD", "info": "Ana Clara Noronha, Arcelor Mittal"},
+				{"salahora": "17:00", "titulo": "Shock between reed valve and piston in FSI simulation of reciprocating compressors", "info": "Eduardo Postingel Falcetti, Tecumseh do Brasil"},
+				{"salahora": "17:20", "titulo": "3D Simulation of a Wind Turbine", "info": "Brenno Martins; Joseph Youssif Saab Junior, Instituto ESSS"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-session",
+			"descricao": "Chambord - Electromagnetics",
+			"salahora": "14:30",
+			"itens": [
+				{"salahora": "16:20", "titulo": "Thermal bump on plane mirror for IPE beamline monochromator - comparison between internal / side water cooling systems", "info": "Artur Clarindo Pinto, LNLS"},
+				{"salahora": "16:40", "titulo": "The use of HFSS for the development of antennas for 5G networks and radars", "info": "Igor Feliciano da Costa, Inatel"},
+				{"salahora": "17:00", "titulo": "Linear actuator analysis and design for valve application using 2D and 3D FEM", "info": "Thiago Akinaga, EMBRACO"},
+				{"salahora": "17:20", "titulo": "How Simulation-Based Digital Twins and the Industrial Internet of Things Can Improve Product and Process Performance", "info": "Carlo Arguinzoni, ANSYS"}
+			]
+		},
+		{
+			"collapsible": true,
+			"class": "ui-be-technical-discussion",
+			"descricao": "Discussão Técnica",
+			"salahora": "17:40",
+			"itens": [
+				{"salahora": "17:40", "titulo": "Technical Discussion Structural Mechanics", "info": "Lorangerie I"},
+				{"salahora": "17:40", "titulo": "Technical Discussion Fluid Dynamics", "info": "Lorangerie II"},
+				{"salahora": "17:40", "titulo": "Technical Discussion Eletromagnetics", "info": "Malmaison"}
+			]
+		}
 	]
 
 }
